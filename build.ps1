e7b372e3e2887dced80d9d93ad6505e7f5fb3a69
Douglas Chen <dougpuob@gmail.com>
2020-05-30 19:13:38 +0800

    Replaced CLANG_LIBS and LLVM_LIBS with the contents in LLVMConfig.cmake and ClangConfig.cmake files.

--- build.ps1
+++ build.ps1
@@ -44,7 +44,7 @@ Push-Location build
 $env:LLVM_VERSION    = '8.0.0'
 if ($IsWindows) {
     $env:LLVM_BUILD_TYPE = 'Release'
-    $env:LLVM_ROOT_DIR   = 'C:\petzone\llvm\llvm-project.git--10.0.0-release-shrinked'
+    $env:LLVM_ROOT_DIR   = 'C:\petzone\llvm\llvm-project.git--8.0.0-release-shrinked'
     
     cmake .. -G "Visual Studio 16 2019"
     cmake --build . --config $env:LLVM_BUILD_TYPE
